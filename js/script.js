6b6de6e0ab35c3b40330f8534cc32f15c897471e
mrchadparkour <cplattkuhn@gmail.com>
2017-04-06 10:35:04 -0600

    Enter no longer submits, Handwritten font added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
-    $("button").click(function() {
-        var item = $("#newTask").val();
-        $('#toDolist').append('<li class="test"><input type="checkbox">' + item + '</li>');
-    });
-    $(document.body).on('click', 'li', function () {
-      $(this).fadeOut(1000, function () {
+$(window).keydown(function(event) {
+    if (event.keyCode == 13) {
+        event.preventDefault();
+        return false;
+        $("button").click(function() {
+            var item = $("#newTask").val();
+            $('#toDolist').append('<li class="test"><input type="checkbox">' + item + '</li>');
+        });
+
+    }); $(document.body).on('click', 'li', function() {
+    $(this).fadeOut(1000, function() {
         if ($(this).find("input").is(':checked')) {
-          $(this).appendTo('#finishedList').fadeIn(2).addClass('done');
-        }else {
-          $(this).appendTo('#toDolist').fadeIn(2).removeClass('done');
+            $(this).appendTo('#finishedList').fadeIn(2).addClass('done');
+        } else {
+            $(this).appendTo('#toDolist').fadeIn(2).removeClass('done');
         }
 
-      });
     });
+});
 });
