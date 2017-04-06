0f9146fa03011bc4559379d33eebe34368732c90
mrchadparkour <cplattkuhn@gmail.com>
2017-04-06 10:47:53 -0600

    broke something, this is my fix

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
-$(window).keydown(function(event) {
-    if (event.keyCode == 13) {
-        event.preventDefault();
-        return false;
-        $("button").click(function() {
-            var item = $("#newTask").val();
-            $('#toDolist').append('<li class="test"><input type="checkbox">' + item + '</li>');
-        });
-
-    }); $(document.body).on('click', 'li', function() {
-    $(this).fadeOut(1000, function() {
+    $("button").click(function() {
+        var item = $("#newTask").val();
+        $('#toDolist').append('<li class="test"><input type="checkbox">' + item + '</li>');
+    });
+    $(document.body).on('click', 'li', function () {
+      $(this).fadeOut(1000, function () {
         if ($(this).find("input").is(':checked')) {
-            $(this).appendTo('#finishedList').fadeIn(2).addClass('done');
-        } else {
-            $(this).appendTo('#toDolist').fadeIn(2).removeClass('done');
+          $(this).appendTo('#finishedList').fadeIn(2).addClass('done');
+        }else {
+          $(this).appendTo('#toDolist').fadeIn(2).removeClass('done');
         }
 
+      });
     });
-});
+
+    $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
 });
