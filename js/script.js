bd8aff1fed3d3b9c61fae7ea2dc7634427c2de7d
kashyab <kashyaboretsky@gmail.com>
2017-04-06 12:09:35 -0600

    added new css and old html

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+     $("button").click(function() {
+         var item = $("#newTask").val();
+         $('#toDolist').append('<li class="test"><input type="checkbox">' + item + '</li>');
+     });
+     $(document.body).on('click', 'li', function () {
+       $(this).fadeOut(1000, function () {
+         if ($(this).find("input").is(':checked')) {
+           $(this).appendTo('#finishedList').fadeIn(2).addClass('done');
+         }else {
+           $(this).appendTo('#toDolist').fadeIn(2).removeClass('done');
+         }
+
+       });
+     });
+ });
